717fd780efbf47c1e4fbb45114e2973477b723b7
Pawat.thr <pawat.th@ku.th>
2021-10-10 15:54:13 +0700

    GoChat

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
-
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/pusher/pusher-http-go"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error{
-		return c.SendString("HELLO WORLD")
+	app.Use(cors.New())
+
+	pusherClient := pusher.Client{
+		AppID:   "1279880",
+		Key:     "3ed0fb6e809d61a3f9c4",
+		Secret:  "09bbe50e378ce0f7d2a0",
+		Cluster: "ap1",
+		Secure:  true,
+	}
+
+	app.Post("/api/messages", func(c *fiber.Ctx) error {
+		var data map[string]string
+
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+
+		pusherClient.Trigger("chat", "message", data)
+
+		return c.JSON([]string{})
 	})
 
 	app.Listen(":8000")
